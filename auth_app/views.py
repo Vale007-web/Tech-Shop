97c8d619d7e073669afb4090118651c624174a45
Vale007-web <valerio.720.g@gmail.com>
2026-01-20 17:59:57 +0100

    Add files via upload

---
+++ auth_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegistrationForm
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def profile_view(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'profile.html')
